b62a9b34274f0f0a437a8bf0dda70fceb1c57d57
Ryan Gens <gensy83@gmail.com>
2017-10-29 12:40:31 -0500

    Initial FileWatcher Utility

---
+++ FileWatcher/FileWatcher/FileFunctions.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Configuration;
+
+
+namespace FileWatcher
+{
+    class FileFunctions
+    {
+        private static FileSystemWatcher fileWatcher;
+        private static String logFilePath = ConfigurationSettings.AppSettings["logFilePath"];     
+
+        public static void initialize()
+        {
+            // Create a file watcher that monitors the log file for changes to lastwrite date-time
+            fileWatcher = new FileSystemWatcher();
+            fileWatcher.Path = Path.GetDirectoryName(logFilePath);
+            fileWatcher.NotifyFilter = NotifyFilters.LastWrite;
+            fileWatcher.Filter = Path.GetFileName(logFilePath);
+            fileWatcher.EnableRaisingEvents = true;
+            fileWatcher.Changed += new FileSystemEventHandler(OnChange);
+            fileWatcher.Created += new FileSystemEventHandler(OnChange);
+
+        }
+
+        private static void OnChange(object source, FileSystemEventArgs e)
+        {
+            // Read The Log File
+            var logEntry = new List<string>();
+            StringBuilder log = new StringBuilder();
+
+            if (File.Exists(logFilePath))
+            {
+                using (Stream stream = File.Open(logFilePath, FileMode.Open, FileAccess.Read,
+                        FileShare.ReadWrite))
+                {
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        var entry = reader.ReadLine();
+
+                        while (entry != null)
+                        {
+                            logEntry.Add(entry);
+                            entry = reader.ReadLine();
+                        }
+                    }
+                }
+                foreach (String entry in logEntry)
+                    log.AppendLine(entry);
+            }
+            else
+            {
+                log.Clear();
+                log.Append(String.Format("The file {0} Can Not Be Found. Please Verify the file path is correct.", logFilePath));
+            }                           
+
+            var f = Application.OpenForms.Cast<Form>().Where(x => x.Name == "Form1").FirstOrDefault();
+
+            // Read each line and call back to the UI thread to change the tb_LogEntry RichTextBox
+            f.Controls.Find("rtb_LogEntry", true).FirstOrDefault().
+                Invoke((MethodInvoker)delegate {
+                    RichTextBox rtb = (RichTextBox)f.Controls.Find("rtb_LogEntry", true).FirstOrDefault();
+                    rtb.Clear();
+                    rtb.Focus();
+                    rtb.AppendText(log.ToString());                    
+            });  
+        }
+    }
+}
---
+++ FileWatcher/FileWatcher/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace FileWatcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtb_LogEntry = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // rtb_LogEntry
+            // 
+            this.rtb_LogEntry.Location = new System.Drawing.Point(2, 3);
+            this.rtb_LogEntry.Name = "rtb_LogEntry";
+            this.rtb_LogEntry.Size = new System.Drawing.Size(270, 246);
+            this.rtb_LogEntry.TabIndex = 0;
+            this.rtb_LogEntry.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.rtb_LogEntry);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtb_LogEntry;
+    }
+}
+
